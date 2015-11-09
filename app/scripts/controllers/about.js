02d73d1c390e44829be4213cb2d1efe5f041e0ef
Daniele Damiani <daniele.damiani@underwriteme.co.uk>
2015-11-09 11:57:11 +0000

    first commit: scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phoneappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phoneappApp
+ */
+angular.module('phoneappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phoneappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the phoneappApp
+ */
+angular.module('phoneappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
