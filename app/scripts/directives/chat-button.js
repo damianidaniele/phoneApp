89d78d6c417b30264b0d7c83413dde9896dc5921
Daniele Damiani <daniele.damiani@underwriteme.co.uk>
2015-11-24 16:34:41 +0000

    clean input and return focus in chat-button directive

--- app/scripts/directives/chat-button.js
+++ app/scripts/directives/chat-button.js
@@ -8,12 +8,18 @@ angular.module('phoneApp')
       templateUrl: '/templates/chat-button.html',
       controller: ['$scope', function ($scope) {
 
+        var inputArea = $("[name='message']");
+
+        $(inputArea).focus();
+
         $scope.send = function(){
           event.preventDefault();
-          var message = $("[name='message']").val();
+          var message = inputArea.val();
           if(message) {
+            inputArea.val('');
             $scope.sendMessage(message);
           }
+          $(inputArea).focus();
         };
       }]
     };
